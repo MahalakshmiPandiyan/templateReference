16e54101d589b7b766a416c9ab5cd3fa8c954d38
MahalakshmiPandiyan <mahapandiyan23@gmail.com>
2022-05-13 15:00:58 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'templateReference';
+  users=[1,2,3]
+  callPhone(phone:string){
+    console.log(phone);
+  }
+  addition(){
+    alert ("HELLO WORLD")
+  }
 }
